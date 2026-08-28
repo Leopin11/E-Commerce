83b1d6866778d180e62b476dab371d57e5a1c097
Leo Pinto <leo071729@gmail.com>
2026-08-27 20:47:27 -0500

    Leonardo pinto creo el proyecto, creo el repositorio  GitHub y hizo el diagrama uml de las clase y sus relaciones

---
+++ src/main/java/domain/User.java
@@ -0,0 +1,34 @@
+package domain;
+
+import java.util.Date;
+
+public class User {
+    private Integer id;
+    private String name;
+    private String phone;
+    private String password;
+    private String address;
+    private Date registrationDate;
+    private int age;
+
+
+    public void create(){
+
+    }
+
+    public void selectById(int id){
+
+    }
+
+    public void selectAll(){
+
+    }
+
+    public void update(){
+
+    }
+
+    public void delete(int id){
+
+    }
+}
